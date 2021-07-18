424d62b09d52fdcf11826121b5b0aa1a25d6853d
OlenaAwd <elena.avdiunina@gmail.com>
2021-07-18 19:07:53 +0300

    finish hw-12

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+
+const URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountries (name) {
+    return fetch(`${URL}/name/${name}`)
+
+.then((response) => response.json());    
+}
+
+export default { fetchCountries };
